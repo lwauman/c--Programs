4a63fc0535f1c5e1a171c64d1b43c586971d8c54
Lucas Auman <lwauman@uncg.edu>
2016-10-16 13:04:37 -0400

    starting C++ programming

---
+++ carrots.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+  using namespace std;
+
+  int carrots;
+
+  carrots = 25;
+  cout << "I have " << carrots << " carrots." << endl;
+
+  carrots = carrots - 1;
+
+  cout << "Now I have " << carrots << " carrots." << endl;
+
+  cin.get();
+  return 0;
+}
---
+++ myFirst.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//comments
+int main(){
+  using namespace std;
+
+  cout << "Hello World";
+  cout << endl;
+  
+  cin.get();
+  return 0;
+}
